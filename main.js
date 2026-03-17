bdd363387c5226c1202799ab8cd307332d1c5af1
schoolTabber <subianvu@gmail.com>
2026-03-16 21:34:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ function getOperater() {
   //Put code here
   return operater;
 }
-function answer() {
+function getAnswer() {
   var result = document.getElementById("answer");
   var number1 = Number(document.getElementById("1").textContent);
   var number2 = Number(document.getElementById("2").textContent);
-  var operater = getOperater
+  var operater = getOperater();
   if (operater === "addition") {
     var answer = number1 + number2;
     result.textContent = answer;
