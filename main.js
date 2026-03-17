82aeeda7900cc7e0cf862e7a28eb556f217342f5
schoolTabber <subianvu@gmail.com>
2026-03-16 19:58:44 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function getOperater() {
+  //Put code here
+  return operater;
+}
+function answer() {
+  var result = document.getElementById("answer");
+  var number1 = Number(document.getElementById("1").textContent);
+  var number2 = Number(document.getElementById("2").textContent);
+  var operater = getOperater
+  if (operater === "addition") {
+    var answer = number1 + number2;
+    var answer = number1 + number2;
+  }
+}
